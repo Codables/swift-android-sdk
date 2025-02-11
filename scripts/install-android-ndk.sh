183311c6e8daf9d1f7fde1b34fbc01fa91c6b723
Jason Foreman <jason@threeve.org>
2025-02-10 23:38:19 -0600

    WIP no container

---
+++ scripts/install-android-ndk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+
+ndk_version=${NDK_VERSION:?android ndk version is required}
+ndk_sha1=${NDK_SHA1:?android ndk sha1sum is required}
+
+ndk_zip=android-ndk-r${ndk_version}-linux.zip;
+curl -s -O "https://dl.google.com/android/repository/${ndk_zip}"
+echo "${ndk_sha1} ${ndk_zip}" | sha1sum -c -
+unzip "${ndk_zip}"
+rm "${ndk_zip}"
+
+echo ANDROID_NDK_ROOT=${PWD}/android-ndk-r${ndk_version}
